7b78517f3e459f54dd39582929eb2452347d40d5
Mohamed khalil <khm40005000@outlook.com>
2024-07-01 01:54:46 +0300

    some changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,9 @@
 # this puppet manifest should install a package
-exec { 'python3-pip':
-  command => '/usr/bin/apt-get install -y python3-pip'
-}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Exec['python3-pip']
+}
+package { 'Werkzeug ':
+  ensure => '2.1.1',
+provider => 'pip3',
 }
