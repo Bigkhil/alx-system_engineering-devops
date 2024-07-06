b825bd5f341a0d72716272eb7a31ec89bc73c220
Mohamed khalil <khm40005000@outlook.com>
2024-07-07 00:23:23 +0300

    some changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,8 @@
 # this manifest changes the ssh client config. file
-$content = 'IdentityFile ~/.ssh/school
+$content = 'Host 485479-web-01
+    HostName 54.152.135.240
+    User ubuntu
+    IdentityFile ~/.ssh/school
     PasswordAuthentication no'
 file { '/etc/ssh/ssh_config':
     ensure  => file
