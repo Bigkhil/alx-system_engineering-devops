9673b6ab25a5f08f85f5e01c1bbc2620a3207ef9
Mohamed khalil <khm40005000@outlook.com>
2024-07-30 17:11:12 +0300

    some changes

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# this script gather some information about an employee from an api
+import sys
+import requests
+
+
+def main():
+    '''this is the main function'''
+    todos = (
+            f"https://jsonplaceholder.typicode.com/todos/?userId={sys.argv[1]}"
+            )
+    employee_url = (
+            f"https://jsonplaceholder.typicode.com/users/?id={sys.argv[1]}"
+            )
+    url_notdone = (
+            f"https://jsonplaceholder.typicode.com/todos/?userId={sys.argv[1]}"
+            "&completed=false"
+            )
+
+    response = requests.get(todos)
+    data = response.json()
+    response = requests.get(url_notdone)
+    no_notdone = len(response.json())
+    response = requests.get(employee_url)
+    employee = response.json()
+    print(
+            f"Employee {employee[0].get('name')} is done with tasks"
+            f"({len(data) - no_notdone}/{len(data)}):"
+            )
+    for job in data:
+        if job.get('completed') is True:
+            print(f"    {job.get('title')}")
+
+
+if __name__ == "__main__":
+    main()
